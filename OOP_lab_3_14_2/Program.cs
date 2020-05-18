6bb0759d10fffea781fa4d907f96d077f8ee47f3
Роман Данилевич <dwordchanel@gmail.com>
2020-05-19 02:45:56 +0300

    Add project files.

---
+++ OOP_lab_3_14_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_14_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sport = new StreamReader("D:/sport.txt");
+
+            string results = sport.ReadToEnd();
+
+            sport.Close();
+
+            StreamWriter sportNew = new StreamWriter("D:/sport.txt", true);
+
+            Console.Write("Iм'я голови журi: ");
+
+            sportNew.Write("\nIм'я голови журi: {0}", Console.ReadLine());
+
+            sportNew.Close();
+        }
+    }
+}
